68970d83f1b89eb0bf106c7637772ed06c50a048
andrey <andreases_mobimoz@ukr.net>
2022-06-02 17:13:23 +0300

    Connected the mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
+const mongoose = require("mongoose")
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const {PORT=3000, DB_HOST} = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .then(() => app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
-});
+})).catch(error => {
+  console.log(error.message)
+  process.exit(1)
+})
